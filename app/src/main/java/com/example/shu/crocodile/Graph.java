f7365005c68d38e431921d0354a8940325ba65d3
shu <pavlovshu@gmail.com>
2017-04-17 20:42:14 +0300

    normal colors normal paint

--- app/src/main/java/com/example/shu/crocodile/Graph.java
+++ app/src/main/java/com/example/shu/crocodile/Graph.java
@@ -6,38 +6,39 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
-/**
- * Created by shu on 02.04.17.
- */
 
 public class Graph {
+
+    public static int count_pix = 20;
+    public static int dp_can = 340;
+    public static int size_pix = dp_can/count_pix;
 
     public static int dpToPx(int dp){
         return(int)(dp* Resources.getSystem().getDisplayMetrics().density);
     }
 
-    public static int pxToDp(int px){
+    public static int pxToDp(float px){
         return(int)(px / Resources.getSystem().getDisplayMetrics().density);
     }
 
 
     public static Bitmap newBitmap (Bitmap b){
-        b = Bitmap.createBitmap(dpToPx(340),dpToPx(340), Bitmap.Config.ARGB_8888);
+        b = Bitmap.createBitmap(dpToPx(dp_can),dpToPx(dp_can), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(b);
         canvas.drawColor(Color.DKGRAY);
 
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.parseColor("white"));
+        paint.setColor(Color.WHITE);
 
         int x = 0;
-        for (int i = 0; i<17; i++){
+        for (int i = 0; i<count_pix; i++){
             int y = 0;
-            for (int j = 0; j<17; j++) {
-                canvas.drawRect(x, y, x + dpToPx(20) - 2, y + dpToPx(20) - 2, paint);
-                y += dpToPx(20);
+            for (int j = 0; j<count_pix; j++) {
+                canvas.drawRect(x+1, y+1, x + dpToPx(size_pix) - 1, y + dpToPx(size_pix) - 1, paint);
+                y += dpToPx(size_pix);
             }
-            x += dpToPx(20);
+            x += dpToPx(size_pix);
         }
 
         return b;
@@ -45,49 +46,48 @@ public class Graph {
 
     public static Bitmap getBitmap(float x, float y, Bitmap b, int c){
 
-        int xd = pxToDp((int)x);
-        xd = dpToPx((xd/20) * 20);
-        int yd = pxToDp((int)y);
-        yd = dpToPx((yd/20) * 20);
+        int xd = pxToDp(x);
+        xd = dpToPx((xd/size_pix) * size_pix);
+        int yd = pxToDp(y);
+        yd = dpToPx((yd/size_pix) * size_pix);
 
         Canvas canvas = new Canvas(b);
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
 
 
-        //TO_DO:normal color
         switch (c){
             case 0: paint.setColor(Color.parseColor("#FF000000"));
                     break;
-            case 1: paint.setColor(Color.parseColor("#FFF17A0A"));
+            case 1: paint.setColor(Color.parseColor("#FFb97a57"));
                     break;
-            case 2: paint.setColor(Color.parseColor("#FFCC0000"));
+            case 2: paint.setColor(Color.parseColor("#FF880016"));
                     break;
-            case 3: paint.setColor(Color.parseColor("red"));
+            case 3: paint.setColor(Color.parseColor("#FFed1b24"));
                     break;
-            case 4: paint.setColor(Color.parseColor("#FFFF4081"));
+            case 4: paint.setColor(Color.parseColor("#FFfeaec9"));
                     break;
-            case 5: paint.setColor(Color.parseColor("#FFFFBB33"));
+            case 5: paint.setColor(Color.parseColor("#FFff7f26"));
                     break;
-            case 6: paint.setColor(Color.parseColor("yellow"));
+            case 6: paint.setColor(Color.parseColor("#FFfef200"));
                     break;
-            case 7: paint.setColor(Color.parseColor("#FF99CC00"));
+            case 7: paint.setColor(Color.parseColor("#FFb5e51d"));
                     break;
-            case 8: paint.setColor(Color.parseColor("#FF669900"));
+            case 8: paint.setColor(Color.parseColor("#FF23b14d"));
                     break;
-            case 9: paint.setColor(Color.parseColor("#FF00DDFF"));
+            case 9: paint.setColor(Color.parseColor("#FF00a3e8"));
                     break;
-            case 10: paint.setColor(Color.parseColor("blue"));
+            case 10: paint.setColor(Color.parseColor("#FF3f47cc"));
                     break;
-            case 11: paint.setColor(Color.parseColor("#FF33B5E5"));
+            case 11: paint.setColor(Color.parseColor("#FF9ad9ea"));
                     break;
-            case 12: paint.setColor(Color.parseColor("#FFAA66CC"));
+            case 12: paint.setColor(Color.parseColor("#FFa349a3"));
                     break;
-            case 13: paint.setColor(Color.parseColor("white"));
+            case 13: paint.setColor(Color.parseColor("#FFffffff"));
                      break;
         }
 
-        canvas.drawRect(xd, yd, xd + dpToPx(20)-2, yd+dpToPx(20)-2, paint);
+        canvas.drawRect(xd+1, yd+1, xd + dpToPx(size_pix)-1, yd+dpToPx(size_pix)-1, paint);
 
         return b;
     }
